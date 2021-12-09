c2419d40c43cd2a34f4bd957704b25236818fb46
Milton <milton761@gmail.com>
2021-12-08 23:03:10 -0500

    kick off

---
+++ src/main/java/RedBlackTree.java
@@ -0,0 +1,61 @@
+
+
+public class RedBlackTree<E extends Comparable<E>> {
+
+    /**
+     * Do not forget add your unit test!
+     *
+     * You can modify this file as your prefer
+     */
+
+    /**
+     * Add value to the tree. Tree can contain multiple equal values.
+     *
+     * @param value to add to the tree.
+     * @return True if successfully added to tree.
+     */
+    public boolean add(E value) {
+        //todo implement here!
+        return false;
+    }
+
+    /**
+     * Remove first occurrence of value in the tree.
+     *
+     * @param value to remove from the tree.
+     * @return T value removed from tree.
+     */
+    public E remove(E value) {
+        //todo implement here!
+        return null;
+    }
+
+    /**
+     * Clear the entire tree.
+     */
+    public void clear() {
+        //todo implement here!
+    }
+
+    /**
+     * Does the tree contain the value.
+     *
+     * @param value to locate in the tree.
+     * @return True if tree contains value.
+     */
+    public boolean contains(E value) {
+        //todo implement here!
+        return false;
+    }
+
+    /**
+     * Get number of nodes in the tree.
+     *
+     * @return Number of nodes in the tree.
+     */
+    public int size() {
+        //todo implement here!
+        return 0;
+    }
+
+}
